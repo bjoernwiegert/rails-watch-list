71808d8e0eb1d93ee8b57f7ebd5fbdeb77e01bf4
Bjoern Wiegert <bwiegert@slb.com>
2022-06-13 15:58:56 +0200

    validation

---
+++ app/models/movie.rb
@@ -0,0 +1,6 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+  validates :title, uniqueness: true
+  validates :title, length: { minimum: 1 }
+  validates :overview, length: { minimum: 1 }
+end
